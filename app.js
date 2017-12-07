89f6d870fdc110079ebac5c88d5c561fb4f11c11
SupineDread <hazielfe@gmail.com>
2017-12-06 18:39:06 -0600

    Vamos por el email

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const request = require('request');
 const moment = require('moment');
 const _ = require('lodash');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
 const app = express();
 
@@ -11,6 +12,14 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('views/assets'));
+
+const transport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'arcaniteamp@gmail.com',
+    pass: 'trxtrxtrx'
+  }
+});
 
 const API_CAPAC = 'http://104.236.144.72/api/v1';
 
@@ -78,7 +87,7 @@ app.post('/send', (req, res) => {
   }, (err, response) => {
     if(err) {
       console.log(err);
-      res.render('index', {err: 'No se pudo enviar el formulario.'});
+      res.render('home', {err: 'No se pudo enviar el formulario.'});
     } else {
       console.log(response);
       res.redirect('/contacto');
